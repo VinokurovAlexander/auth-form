f97d32c4023a577f22f211e3f028deed36074ecd
Alexander Vinokurov <vinokurov.ag@mail.ru>
2022-12-13 17:11:44 +0300

    edit auth state

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from "vite";
 import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
 import { resolve } from 'path';
 
 export default defineConfig({
     plugins: [
-        react()
+        react(),
+        svgr({
+            exportAsDefault: true
+        }),
     ],
     resolve: {
         alias: {
